0b3d7db22a5b547ff6cecadfd00c4e2667e7cfb0
MC Saeid <63400670+mcsaeid@users.noreply.github.com>
2022-01-27 23:35:05 +0330

    Update url.py

--- url.py
+++ url.py
@@ -1,11 +1,11 @@
 import webbrowser
 
 while True:
-    old_url = input('Enter book url = ')
+    old_url = input('Enter the book URL: ')
 
     uuid = old_url.split('/')[-1]
 
     new_url = fr"https://digital.darkhorse.com/api/v6/book/{uuid}"
 
-    print('Opening the download adress...')
+    print('Opening the download address...')
     webbrowser.open(new_url)
